606047f6e60177503344ab29a13eb7abd453a786
Escape <imjihyeon00@gmail.com>
2021-09-02 12:20:42 +0900

    2021.09.02

---
+++ refer-effect/assets/js/custom.js
@@ -0,0 +1,10 @@
+
+    document.querySelector(".info button").addEventListener("click", function(){
+       document.querySelector("#modal").classList.add("show"); 
+       document.querySelector("#modal").classList.remove("hide"); 
+    });
+
+    document.querySelector(".modal-cont button").addEventListener("click", function(){
+       document.querySelector("#modal").classList.add("hide"); 
+    });
+    
