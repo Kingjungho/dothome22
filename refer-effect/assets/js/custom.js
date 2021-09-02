a235d72f9ecc0b30dab700ac126ee558357c9ad7
akside111 <87642334+wow2002r@users.noreply.github.com>
2021-09-02 15:57:48 +0900

    2021.09.02

--- refer-effect/assets/js/custom.js
+++ refer-effect/assets/js/custom.js
@@ -8,3 +8,31 @@
        document.querySelector("#modal").classList.add("hide"); 
     });
     
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+      document.querySelectorAll('pre code').forEach((el) => {
+          hljs.highlightElement(el);
+      });
+  });
+
+
+
+  //Code tab menu
+
+  const tabBtn1 = document.querySelectorAll(".code-view .view-title li a");
+  const tabCont1 = document.querySelectorAll(".language-html .hljs ");
+
+  tabBtn1.forEach((element) => {
+   element.addEventListener("click", function(){
+       
+       tabBtn1.forEach(el => {
+           el.classList.remove("active")
+       });
+       element.classList.add("active")
+
+       tabCont1.forEach( el => {
+           el.style.display = "none";
+       })
+       tabCont1[index].style.display ="block";
+   })
+});
