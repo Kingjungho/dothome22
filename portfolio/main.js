266b2e4ef4ee4cfd8092c398a98cf64b7b514731
Kingjungho <dkdlwo@naver.com>
2021-11-18 01:19:48 +0900

    2021.11.18

--- portfolio/main.js
+++
@@ -1,8 +0,0 @@
-const section = document.querySelector("#section4");
-
-section.addEventListener('scroll', function() {
-    if (document.section.scrollTop > 0) {
-      let currScrollPos2 = document.section.scrollTop;
-      section.style.opacity = -currScrollPos2 / 400 + 2;
-    }
-  });
