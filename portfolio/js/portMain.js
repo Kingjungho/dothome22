a83b304860a900180b7ac45bbc34becc9c05faf5
Kingjungho <dkdlwo@naver.com>
2021-11-25 14:30:13 +0900

    2021.11.25

--- portfolio/js/portMain.js
+++ portfolio/js/portMain.js
@@ -119,18 +119,64 @@
     contact.addEventListener("click", contactClickhandler)
     contactMe.addEventListener("click", contactMeClickhandler)
 
+    // 아코디언 메뉴
+    let accordionBtn = document.querySelectorAll('#section10 .contactTable .tableWrap li h4');
+    let allTexts = document.querySelectorAll('.text');
+    let accIcon = document.querySelectorAll('#section10 .contactTable .tableWrap li .arrow');
+
+
+    accordionBtn.forEach(function (el) {
+        el.addEventListener('click', toggleAccordion)
+    });
+
+
+    function toggleAccordion(el) {
+        let targetText = el.currentTarget.nextElementSibling.classList;
+        let targetAccIcon = el.currentTarget.children[0];
+        let target = el.currentTarget;
+
+        if (targetText.contains('show')) {
+        targetText.remove('show');
+        targetAccIcon.classList.remove('anime');
+        target.classList.remove('accordionTitleActive');
+        } 
+        else {
+            accordionBtn.forEach(function (el) {
+            el.classList.remove('accordionTitleActive');
+            
+            allTexts.forEach(function (el) {
+                el.classList.remove('show');
+            })
+            
+            accIcon.forEach(function (el) {
+                el.classList.remove('anime');
+            }) 
+        })  
+        
+            targetText.add('show');
+            target.classList.add('accordionTitleActive');
+            targetAccIcon.classList.add('anime');
+        }  
+    }
+
 
     // scroll Trigger
     const trigger = new ScrollTrigger.default({
         trigger: {
             once: false,
             offset: {
-                element: {
+                // element: {
+                //     x: 0,
+                //     y: (trigger, rect, direction) => {
+                //         return 0.2
+                //     }
+                // },
+                viewport: {
                     x: 0,
-                    y: (trigger, rect, direction) => {
-                        return 0.5
+                    y: (trigger, frame, direction) => {
+                        return trigger.visible ? 0 : 0.5
                     }
-                },
+                }
             },
         }
     });
