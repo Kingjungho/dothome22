32f11304dfde84513330e5204ee939f5cdbeff1f
Kingjungho <dkdlwo@naver.com>
2021-12-15 03:22:14 +0900

    2021.12.15

--- zhomeTest/game-refactoring/src/field.js
+++ zhomeTest/game-refactoring/src/field.js
@@ -1,32 +1,41 @@
 'use strict';
 
-const carrotSound = new Audio('./sound/carrot_pull.mp3');
-const CARROT_SIZE = 80;
+import * as sound from './sound.js';
 
-export default class Field {
+const CARROT_SIZE = 80;
+const FIELD_TOP_PADDING = 50;
+
+export const ItemType = Object.freeze({
+  carrot: 'carrot',
+  bug: 'bug',
+});
+
+export class Field {
   constructor(carrotCount, bugCount) {
     this.carrotCount = carrotCount;
     this.bugCount = bugCount;
     this.field = document.querySelector('.game__field');
     this.fieldRect = this.field.getBoundingClientRect();
-    this.field.addEventListener("click", this.onClick);
+    this.onFieldClickListener = this.onFieldClickListener.bind(this);
+    this.field.addEventListener('click', this.onFieldClickListener);
   }
 
   init() {
     this.field.innerHTML = '';
-    this._addItem('carrot', this.carrotCount, 'img/carrot.png');
-    this._addItem('bug', this.bugCount, 'img/bug.png');
+    this.addItem(this.carrotCount, 'img/carrot.png', 'carrot');
+    this.addItem(this.bugCount, 'img/bug.png', 'bug');
   }
 
-  setClickListener(onItemClick) {
+  setItemClickListener(onItemClick) {
     this.onItemClick = onItemClick;
   }
 
-  _addItem(className, count, imgPath) {
+  addItem(count, imgPath, className) {
     const x1 = 0;
-    const y1 = 0;
     const x2 = this.fieldRect.width - CARROT_SIZE;
-    const y2 = this.fieldRect.height - CARROT_SIZE;
+    const y1 = this.field.offsetTop + FIELD_TOP_PADDING;
+    const y2 = this.field.offsetTop + this.fieldRect.height - CARROT_SIZE;
+    console.log(this.fieldRect.height)
     for (let i = 0; i < count; i++) {
       const item = document.createElement('img');
       item.setAttribute('class', className);
@@ -36,27 +45,22 @@ export default class Field {
       const y = randomNumber(y1, y2);
       item.style.left = `${x}px`;
       item.style.top = `${y}px`;
+      item.style.userDrag = 'none';
       this.field.appendChild(item);
     }
   }
 
-
-  onClick = (event) => {
+  onFieldClickListener(event) {
     const target = event.target;
     if (target.matches('.carrot')) {
+      sound.playCarrot();
       target.remove();
-      playSound(carrotSound);
-      this.onItemClick && this.onItemClick('.carrot');
+      this.onItemClick && this.onItemClick(ItemType.carrot);
+      console.log(this.onItemClick)
     } else if (target.matches('.bug')) {
-      this.onItemClick && this.onItemClick('.bug');
+      this.onItemClick && this.onItemClick(ItemType.bug);
     }
   }
-}
-
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
 }
 
 function randomNumber(min, max) {
