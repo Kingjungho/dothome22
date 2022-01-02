71a425be0cac6e5a40b72f432e4d8538a87a00b1
Kingjungho <dkdlwo@naver.com>
2022-01-03 01:06:59 +0900

    2022.01.03

--- zhometestSelfCheck/paintJs/app.js
+++ zhometestSelfCheck/paintJs/app.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 500;
+canvas.height = 500;
+
+
 ctx.strokeStyle = "#2c2c2c"
 ctx.lineWidth = 2.5;
 
@@ -17,16 +21,20 @@ function startPainting(){
 function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y)
-}
-
-function onMouseDown(event){
-  painting = true;
+  if(!painting){
+    console.log("a", x, y)
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+  } else {
+    console.log("b", x, y)
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove)
   canvas.addEventListener("mousedown", startPainting)
   canvas.addEventListener("mouseup", stopPainting)
-  canvas.addEventListener("mouseleave", onMouseLeave)
+  canvas.addEventListener("mouseleave", stopPainting)
 }
