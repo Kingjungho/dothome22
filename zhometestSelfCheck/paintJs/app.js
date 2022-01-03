99e0adbeca14a0585dc60ffb922c7ecca39ee46c
Kingjungho <dkdlwo@naver.com>
2022-01-04 01:09:35 +0900

    2022.01.04

--- zhometestSelfCheck/paintJs/app.js
+++ zhometestSelfCheck/paintJs/app.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 const color = document.querySelector(".controls__colors");
+const range = document.querySelector("#jsRange");
 const fill = document.querySelector("#jsMode");
 const saveImage = document.querySelector("#jsSave");
 
@@ -9,7 +10,8 @@ let CANVAS_SIZE = 500;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
-
+ctx.strokeStyle = "#000000"
+ctx.fillStyle = "#000000"
 ctx.lineWidth = 2.5;
 
 
@@ -36,30 +38,44 @@ function onMouseMove(event){
   }
 }
 
+const handleCanvasFiling = () => {
+  if(filling){
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+  }
+}
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove)
   canvas.addEventListener("mousedown", startPainting)
   canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
+  canvas.addEventListener("click", handleCanvasFiling)
 }
 
 const colorChange = e => {
-  const target = e.target.dataset.id
+  const target = e.target.style.backgroundColor;
   console.log(target)
   if(target){
     ctx.strokeStyle = `${target}`
+    ctx.fillStyle = ctx.strokeStyle
   }
 }
 
 const fillCanvas = () => {
-  if(filling){
-    filling = false;
-    fill.innerText = "Reset"
+  if(filling === true){
+    filling = false;  
+    fill.innerText = "FILL"
   } else {
     filling = true;
-    fill.innerText = "FILL"
+    fill.innerText = "PAINT"
   }
+}
+
+const changeRange = e => {
+  size = e.target.value;
+  ctx.lineWidth = size;
 }
 
 color.addEventListener("click", colorChange)
 fill.addEventListener("click", fillCanvas)
+range.addEventListener("input", changeRange)
